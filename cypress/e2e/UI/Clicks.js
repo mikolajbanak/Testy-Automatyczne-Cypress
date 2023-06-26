b4c559dc17676863672911304bff92f81f50fc90
mbanak265@gmail.com <87323936+MiksonHS@users.noreply.github.com>
2023-06-26 08:05:17 +0200

    test

--- cypress/e2e/UI/Clicks.js
+++ cypress/e2e/UI/Clicks.js
@@ -5,6 +5,7 @@ it("Clicking on element", () =>{
     cy.visit("/");
     cy.contains('[title="Contact us"]' ,'Contact us').click();
     cy.get('[title="Return to Home"]').click();
+    //test
     
 })
 
